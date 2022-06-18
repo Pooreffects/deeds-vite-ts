3c1bce1e7c37ea4c871fbd194a9c6043c9f7e08a
Pooreffects <pooreffects@gmail.com>
2022-06-18 05:36:38 +0100

    routing and transition animation for trendy and search

--- src/components/SearchedGifs.tsx
+++ src/components/SearchedGifs.tsx
@@ -1,15 +1,59 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import gsap from 'gsap';
+import { useEffect, useRef } from 'react';
+interface Props {}
 
-type Props = {};
-
-export default function SearchedGifs({}: Props) {
+export default function SearchedGifs({}: Props): JSX.Element {
+  let linkRef = useRef(null);
+  let searchHeading = useRef(null);
+  useEffect(() => {
+    gsap.fromTo(
+      linkRef.current,
+      {
+        opacity: 0,
+        x: -40,
+      },
+      {
+        opacity: 1,
+        x: 0,
+        duration: 0.7,
+        ease: 'back.out(1)',
+      }
+    );
+    gsap.fromTo(
+      searchHeading.current,
+      {
+        opacity: 0,
+        x: -100,
+      },
+      {
+        opacity: 1,
+        x: 0,
+        duration: 0.5,
+        delay: 0.4,
+        ease: 'back.out(1)',
+      }
+    );
+  }, []);
   return (
-    <div>
-      {/* Input */}
-      <div>
-        <input type="text" placeholder="Search some shit" />
+    <div className=" bg-gray-400 border-x border-darkBlue-100 p-4  h-screen container mx-auto w-full">
+      <div
+        className="font-style text-white text-xl hover:text-darkBlue-100"
+        ref={linkRef}
+      >
+        <Link to="/">Home</Link>
       </div>
+      <main className="flex flex-col items-center justify-evenly pt-8">
+        <h1
+          ref={searchHeading}
+          className="text-7xl font-style font-bold text-darkBlue-100"
+        >
+          Search
+        </h1>
+      </main>
+      <section></section>
       {/* Cards Container 
+            - Input
             - Cards
             - Show More/Show Less 
         */}
--- src/components/TrendyGifs.tsx
+++ src/components/TrendyGifs.tsx
@@ -1,8 +1,57 @@
-interface Props {};
+import { useEffect, useRef } from 'react';
+import gsap from 'gsap';
+import { Link } from 'react-router-dom';
+interface Props {}
 
-export default function TrendyGifs({}: Props) {
+export default function TrendyGifs({}: Props): JSX.Element {
+  let trendyLinkRef = useRef(null);
+  let trendyHeading = useRef(null);
+  useEffect(() => {
+    gsap.fromTo(
+      trendyLinkRef.current,
+      {
+        opacity: 0,
+        x: -40,
+      },
+      {
+        opacity: 1,
+        x: 0,
+        duration: 0.7,
+        ease: 'back.out(1)',
+      }
+    );
+    gsap.fromTo(
+      trendyHeading.current,
+      {
+        opacity: 0,
+        x: -100,
+      },
+      {
+        opacity: 1,
+        x: 0,
+        duration: 0.5,
+        delay: 0.4,
+        ease: 'back.out(1)',
+      }
+    );
+  }, []);
   return (
-    <div>
+    <div className=" bg-gray-400 border-x border-darkBlue-100 p-4  h-screen container mx-auto w-full">
+      <div
+        ref={trendyLinkRef}
+        className="font-style text-white text-xl hover:text-darkBlue-100"
+      >
+        <Link to="/">Home</Link>
+      </div>
+      <main className="flex flex-col items-center justify-evenly pt-8">
+        <h1
+          ref={trendyHeading}
+          className="text-7xl font-style font-bold text-darkBlue-100"
+        >
+          Trending
+        </h1>
+      </main>
+      <section></section>
       {/* Cards Container 
             - Cards
             - Show More/Show Less 
